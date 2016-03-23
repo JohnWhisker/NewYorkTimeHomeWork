f81ab669dfac5903d24bc0702735cc1d05bbd135
Dang Khoi Nguyen <nguyen.dk0952@sinhvien.hoasen.edu.vn>
2016-03-24 00:15:45 +0700

    Update: README update, share function

--- README.md
+++ README.md
@@ -23,8 +23,8 @@ The following **optional** features are implemented:
 
 * [X] Implements robust error handling, [check if internet is available](http://guides.codepath.com/android/Sending-and-Managing-Network-Requests#checking-for-network-connectivity), handle error cases, network failures
 * [X] Used the **ActionBar SearchView** or custom layout as the query box instead of an EditText
-* [ ] User can **share an article link** to their friends or email it to themselves
-* [ ] Replaced Filter Settings Activity with a lightweight modal overlay
+* [X] User can **share an article link** to their friends or email it to themselves
+* [X] Replaced Filter Settings Activity with a lightweight modal overlay
 * [X] Improved the user interface and experiment with image assets and/or styling and coloring
 
 The following **bonus** features are implemented:
--- app/src/main/java/com/example/johnw/nytime/Activities/ArticleActivity.java
+++ app/src/main/java/com/example/johnw/nytime/Activities/ArticleActivity.java
@@ -1,12 +1,18 @@
 package com.example.johnw.nytime.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import com.example.johnw.nytime.Types.Article;
 import com.example.johnw.nytime.R;
+import com.example.johnw.nytime.Types.Article;
 
 public class ArticleActivity extends AppCompatActivity {
 
@@ -26,6 +32,21 @@ public class ArticleActivity extends AppCompatActivity {
             }
         });
         webview.loadUrl(article.getWeburl());
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_article, menu);
+        MenuItem item = menu.findItem(R.id.Share);
+        ShareActionProvider miShare = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        WebView wvArticle = (WebView) findViewById(R.id.wvArticle);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, wvArticle.getUrl());
+        if(miShare!= null) {
+            miShare.setShareIntent(shareIntent);
+        }
+        return super.onCreateOptionsMenu(menu);
     }
 
 }
